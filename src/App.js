4f81808b7bf40e18ef701cedd10bc93fa85d2ac3
ywq <ywq33810536@163.com>
2018-03-08 18:12:23 +0800

    2018-3-8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'antd';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">欢迎使用爱青岛数据服务展示平台</h1>
         </header>
+        <Button type="primary">Button</Button>
         <p className="App-intro">
           这是一个假消息
         </p>
